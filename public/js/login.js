0a606061528b5fc99f592b23cd214cb0d3934402
Blickenderfer <ecblickenderfer@gmail.com>
2023-07-01 12:41:40 -0400

    fixed syntax

--- public/js/login.js
+++ public/js/login.js
@@ -1,20 +1,22 @@
-const loginFormHandler = async e => {
-    e.preventDefault()
-    const username = document.getElementById("username-login").ariaValueMax.trim()
-    const password = document.getElementById("password-login").ariaValueMax.trim()
-    if (username && password){
-        const response = await fetch("/api/users/login", {
-            method: "POST",
-            body: JSON.stringify({username, password}), 
-            headers: {"Content-Type": "application/json"}
-        })
-        console.log(response)
-        if(response.ok){
-            document.location.replace("/home")
-        } else {
-            alert(response.statusText)
-        }
-    }
+// const loginFormHandler = async e => {
+    const loginFormHandler = () => {
+    // e.preventDefault()
+    // const username = document.getElementById("username-login").ariaValueMax.trim()
+    // const password = document.getElementById("password-login").ariaValueMax.trim()
+    // if (username && password){
+    //     const response = await fetch("/api/users/login", {
+    //         method: "POST",
+    //         body: JSON.stringify({username, password}), 
+    //         headers: {"Content-Type": "application/json"}
+    //     })
+    //     console.log(response)
+    //     if(response.ok){
+    //         document.location.replace("/home")
+    //     } else {
+    //         alert(response.statusText)
+    //     }
+    // }
+    console.log("YIKES")
 }
-
-document.querySelector(".login-form").addEventListener("submit", loginFormHandler)
+loginFormHandler()
+// document.querySelector(".login-form").addEventListener("submit", loginFormHandler)
