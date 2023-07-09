dca7c3dd48b6c032206ba70ff3b6c68454c57c3c
Pandax19 <ajwhitley@comcast.net>
2023-07-09 17:55:26 -0400

    flipped schema order

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,6 @@
-DROP DATABASE IF EXISTS event_db;
-CREATE DATABASE event_db;
 
 DROP DATABASE IF EXISTS users_db;
 CREATE DATABASE users_db;
 
-USE users_db;
-
-CREATE TABLE users (
-
-    id INT,
-    name VARCHAR(10)
-
-)
-
+DROP DATABASE IF EXISTS event_db;
+CREATE DATABASE event_db;
